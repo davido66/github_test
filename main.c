fda9cb554d957a93dc2e9cd3cc836f06804d0f4a
dast <davidsto@mail.amis.net>
2017-07-03 23:06:45 +0200

    upgrade

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include "stdio.h"
 
+static int add(int a, int b);
+
 /*main loop*/
 int main(void)
 {
   printf("Test repo");
+  add(5, 8);
+
   return 0;
 }
 
-int add(int a, int b)
+static int add(int a, int b)
 {
   return a+b;
 }
